60f8d3715c8256ca7027baf1dff7142f6b000c03
Jose Vitor Soares <jose.vitor.clashbr@gmail.com>
2020-12-17 23:57:37 -0300

    Comentando o bloco /home#

--- app.js
+++ app.js
@@ -16,44 +16,43 @@ app.get('/login', (req, res) => {
      res.render('login')
 })
 
-app.get('/home#', (req, res) => {
-    var user = "jose.vitor.soares@hotmail.com"
-    var password = "sayajinblue"
+// app.get('/home#', (req, res) => {
+//     var user = "jose.vitor.soares@hotmail.com"
+//     var password = "sayajinblue"
     
-    document.getElementById("error-e.mail").style.display = "none"
-    document.getElementById("error-pass").style.display = "none"
+//     document.getElementById("error-e.mail").style.display = "none"
+//     document.getElementById("error-pass").style.display = "none"
 
-    if(isEmpety() == true){
-        var chave = document.getElementById("error-pass")
-        chave.innerHTML = "Algum campo está vazio"
-        chave.style.display = "block"
-    } else {
-        var usuario = req.body.login
-        var senha = req.body.password
+//     if(isEmpety() == true){
+//         var chave = document.getElementById("error-pass")
+//         chave.innerHTML = "Algum campo está vazio"
+//         chave.style.display = "block"
+//     } else {
+//         var usuario = req.body.login
+//         var senha = req.body.password
 
-        if(user != usuario && password != senha){
-            document.getElementById("error-e.mail").style.display = "flex"
-            document.getElementById("error-pass").innerHTML = "Senha incorreta"
-            document.getElementById("error-pass").style.display = "flex"
-        } else if(user != usuario){
-            document.getElementById("error-e.mail").style.display = "flex"
-        } else if(password != senha){
-            document.getElementById("error-pass").innerHTML = "Senha incorreta"
-            document.getElementById("error-pass").style.display = "flex"
-        } else{
-            res.render('/')
-        }
-    }
+//         if(user != usuario && password != senha){
+//             document.getElementById("error-e.mail").style.display = "flex"
+//             document.getElementById("error-pass").innerHTML = "Senha incorreta"
+//             document.getElementById("error-pass").style.display = "flex"
+//         } else if(user != usuario){
+//             document.getElementById("error-e.mail").style.display = "flex"
+//         } else if(password != senha){
+//             document.getElementById("error-pass").innerHTML = "Senha incorreta"
+//             document.getElementById("error-pass").style.display = "flex"
+//         } else{
+//             res.render('/')
+//         }
+//     }
 
-    function isEmpety(){
-        if(req.body.login == "" || req.body.password == ""){
-            return true
-        } else {
-            return false
-        }
-    }
-    res.render('login')
-})
+//     function isEmpety(){
+//         if(req.body.login == "" || req.body.password == ""){
+//             return true
+//         } else {
+//             return false
+//         }
+//     }
+// })
 
 app.listen(port, () => {
 
